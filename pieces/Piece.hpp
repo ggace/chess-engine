a10fe0ed45f9bc5fff3bbf9049ba4e44f904f547
ggace <60254999+ggace@users.noreply.github.com>
2026-02-23 06:46:56 +0000

    Piece.hpp, Util.cpp 주석 추가

--- pieces/Piece.hpp
+++ pieces/Piece.hpp
@@ -4,15 +4,31 @@
 #include "../util/Position.hpp"
 
 class Piece {
-private:
+protected:
     Type type;
     Turn owner;
     Position position;
     bool is_moved;
 public:
+    
     Piece(Turn own, int row, int col);
 
+    virtual ~Piece() = default;
+
     void moveTo(int row, int col);
-    vector<Position> getPossibleMovement();
-    string toString();
+
+    // 오버라이딩 필요. 각각마다 다름.
+    virtual vector<Position> getPossibleMovement() const =0;
+    
+    // getter
+    Type getType() const;
+    Position getPosition() const;
+    
+    // value와 구분한 이유: 
+    // - moveTo를 실행했을때 is_moved를 바꾸고 다른 경우 is_moved를 변경할 이유가 없음. 
+    // - undo 정도만으로 바꿀수 있게 해야하기 때문에 실제 변수 접근은 private
+    bool isMoved() const;; 
+
+    
+    string toString() const; // json 형식
 };
--- util/Util.hpp
+++ util/Util.hpp
@@ -8,10 +8,13 @@
 
 using namespace std;
 
+// 현재의 턴 순서 or 기물의 소유자를 표시하기 위함.
 enum Turn{
     WHITE, BLACK
 };
 
+// 기물의 종류를 위함. 
+// 실제 구동에서 필요한 것이 아닌 toString과 같이 정보를 출력할때 필요함.
 enum Type {
     KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN
 };
